09bee45ce685176efcd4246c54f8a2e45f2a7041
Martin Ojeda <88889204+mart0302@users.noreply.github.com>
2025-05-21 23:08:49 -0600

    Update flask_mis_api.py

--- flask_mis_api.py
+++ flask_mis_api.py
@@ -76,6 +76,10 @@ def compute_mis():
     G.add_nodes_from(nodes)
     G.add_edges_from(edges)
 
+    is_planar, embedding = nx.check_planarity(G)
+    if not is_planar or len(embedding.faces()) > 1:
+        return jsonify({'error': 'El grafo no es outerplanar'}), 400
+
     mis = compute_mis_outerplanar(G)
     return jsonify({'mis': mis})
 
